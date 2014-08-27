14ec115bb19861af0c1086760eba1364380d848c
JELGT2011 <jelgt2011@gmail.com>
2014-08-27 00:34:17 -0400

    from prototype p2

---
+++ Assets/Test Scripts/PongPlayer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PongPlayer : Photon.MonoBehaviour {
+
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (photonView.isMine)
+        {
+            InputMovement();
+        }
+	}
+
+    void InputMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidbody.MovePosition(rigidbody.position + Vector3.up * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigidbody.MovePosition(rigidbody.position + Vector3.down * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody.MovePosition(rigidbody.position + Vector3.left * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody.MovePosition(rigidbody.position + Vector3.right * speed * Time.deltaTime);
+        }    
+    }
+}
